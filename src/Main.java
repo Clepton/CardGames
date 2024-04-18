30f3bf4c953cd0dd469bc49ea5c69cc101ac281b
Clepton <ldvc.bld@gmail.com>
2024-04-18 16:12:57 +0200

    Setscore

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,16 @@
 public class Main {
     public static void main(String[] args) {
+        int s=0;
         Deck deck = new Deck(); // Crée un jeu de cartes
         Hand hand = new Hand(deck); // Crée une main de cartes à partir du jeu
+        Score score = new Score(s);
         // Affiche la main de cartes
         hand.displayHand();
         deck.displayDeck();
+
+        score.setScore(s);
+
+        System.out.println(score.getScore());
 
 
 
