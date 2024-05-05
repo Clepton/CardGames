0899557865c6accb2466d7bc845022e7bfb537fe
malo9 <malo9.code@gmail.com>
2024-05-05 15:36:56 +0200

    main loop

--- src/Main.java
+++ src/Main.java
@@ -4,34 +4,44 @@ public class Main {
     public static void main(String[] args) {
 
 
-        int loop=1;
+        int s=0;
 
-        int s = 0;
+        String loop = "O";
 
         Deck deck = new Deck(); // Crée un jeu de cartes
         Hand hand = new Hand(deck); // Crée une main de cartes à partir du jeu
         Score score = new Score(s);
 
-        while (loop==1) {
-        // Affiche la main de cartes
-        hand.displayHand();
 
-        score.setScore(s);
 
-        System.out.println(score.getScore());
+        while ("O".equalsIgnoreCase(loop)) {
+
+
+
+
+            // Affiche la main de cartes
+            hand.displayHand();
+
+            score.setScore(s);
+
+            System.out.println(score.getScore());
             Scanner scanner = new Scanner(System.in);
 
+
+
             // Demander à l'utilisateur d'entrer du texte
-            System.out.print("Continuer a jouer ? O/N : ");
-            String texte = scanner.nextLine();
-
-        hand.discardedCard(deck);
+            System.out.print("Continuer à jouer ? O/N : ");
+            loop = scanner.nextLine();
 
 
-        hand.displayHand();
+            while (!loop.equalsIgnoreCase("O") && !loop.equalsIgnoreCase("N")) {
+                System.out.println("Erreur : Veuillez entrer O ou N");
+                loop = scanner.nextLine();
+            }
 
-
-        }
 
     }
+
+}
+
 }
