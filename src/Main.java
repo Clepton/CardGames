5e58874b0a340eeb67734687f4bfbb0c0c8a1546
malo9 <malo9.code@gmail.com>
2024-05-05 15:22:53 +0200

    main loop

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
@@ -10,13 +12,18 @@ public class Main {
         Hand hand = new Hand(deck); // Crée une main de cartes à partir du jeu
         Score score = new Score(s);
 
-        //while (loop==1) {
+        while (loop==1) {
         // Affiche la main de cartes
         hand.displayHand();
 
         score.setScore(s);
 
         System.out.println(score.getScore());
+            Scanner scanner = new Scanner(System.in);
+
+            // Demander à l'utilisateur d'entrer du texte
+            System.out.print("Continuer a jouer ? O/N : ");
+            String texte = scanner.nextLine();
 
         hand.discardedCard(deck);
 
@@ -24,7 +31,7 @@ public class Main {
         hand.displayHand();
 
 
-        //}
+        }
 
     }
 }
