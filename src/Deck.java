5fbebfc67286ae0dbe3e41e876d4231fa2625dce
LeL2BLR <78683729+Clepton@users.noreply.github.com>
2024-05-13 10:04:39 +0200

    Update Deck.java

--- src/Deck.java
+++ src/Deck.java
@@ -1,51 +1,32 @@
 import java.util.ArrayList;
-import java.util.Collections;
 
 public class Deck {
+    private ArrayList<Card> deck;
+
     private static final int NB_CARDS = 52;
 
-    private ArrayList<Card> cards;
-
     public Deck() {
-        this.cards = new ArrayList<Card>();
-        // Construire le jeu de cartes, pour chaque nombre de 2 à 14 (inclus)..
-        for (int number = 1; number <= 13; number++) {
-            String[] suits = {"CLOVER", "HEART", "SPIKE", "DIAMOND"};
-            for (String suit : suits) {
-                String faceCard = verifyName(number); // Obtenir le nom de la carte face
-                cards.add(new Card(number, suit)); // Ajouter la nouvelle carte au jeu
+        deck = new ArrayList<Card>();
+
+        String[] Number = {"A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
+        String[] Suit = {"C", "D", "H", "S"};
+
+        for (String suit : Suit) {
+            for (String number : Number) {
+                Card card = new Card(number, suit);
+                deck.add(card);
             }
         }
-        // Mélanger le jeu de cartes pour que les cartes aient un nouvel ordre
-        Collections.shuffle(cards);
     }
 
-    // Méthode auxiliaire pour vérifier le nom de la carte face en fonction du nombre
-    private String verifyName(int number) {
-        switch (number) {
-            case 11:
-                return "Valet";
-            case 12:
-                return "Roi";
-            case 13:
-                return "Reine";
-            case 14:
-                return "As";
-            default:
-                return String.valueOf(number);
-        }
-    }
-
-    // Méthode pour obtenir le jeu de cartes
     public ArrayList<Card> getCards() {
-        return cards;
+        return deck;
     }
 
-    // Méthode pour afficher le jeu de cartes
     public void displayDeck() {
         System.out.println("Jeu de cartes :");
-        for (Card card : cards) {
-            System.out.println(card.getNumber() + card.getSymbol(card.getSuit()));
+        for (Card card : deck) {
+            System.out.println(card.getNumber() + card.getSuit());
         }
     }
 }
